3f6e3dc1cc9d0774ce4aa086ecaf174d4bcc99fa
manikerisaurabh <manikerisaurabh@gmail.com>
2023-12-06 19:40:08 +0530

    06-dec-2023

---
+++ Dec-2023/5-dec-2023.c
@@ -0,0 +1,45 @@
+/*
+============================== MAX PRODUCT  =================================
+
+Problem statement : Given an array with length 'N' (may includes positive, 
+                    negative, zero) returnt the maximum product subarray
+
+======================================================================================
+
+Sample Input :  A[] = {6, -3, -10, 0, 2}
+
+Sample Output : 180
+
+Explanation : [6, -3, -10] product is : 180
+=====================================================================================
+*/
+
+
+#include<stdio.h>
+#include<math.h>
+int maxProduct(int a[], int n) {
+  int product=0;
+  for(int i=1; i<n-1; i++) {
+    int temp = 1;
+    for(int j=0; j<i; j++) {
+      temp = temp*abs(a[j]);
+    }
+    if(product<temp) {
+      product = temp;
+    }
+  }
+  return product;
+}
+int main(int argc, char const *argv[])
+{
+  int n;
+  printf("Enter the size of array : ");
+  scanf("%d",&n);
+  int a[n];
+  printf("Enter %d elements in array ",n);
+  for(int i=0; i<n; i++) {
+    scanf("%d",&a[i]);
+  }
+  printf("%d is the max sum",maxProduct(a,n));
+  return 0;
+}
